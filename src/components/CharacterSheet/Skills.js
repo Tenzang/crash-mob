252f33cc00975b0ee61d102f39ca59332dd9a28e
Loden <loden.gendun@students.mq.edu.au>
2022-04-30 18:31:47 +1000

    basic character sheet ability & skill display components

---
+++ src/components/CharacterSheet/Skills.js
@@ -0,0 +1,17 @@
+function Skills(props) {
+    const { abilities, skills, proficiency } = props;
+    return (
+        <div>
+            { Object.keys(skills).map( skillName => { return (
+                <div>
+                    { skillName }
+
+                    { abilities[skills[skillName].ability].modifier + (skills[skillName].proficient * proficiency) >= 0 ? '+' : ''}
+                    { abilities[skills[skillName].ability].modifier + (skills[skillName].proficient * proficiency) }
+                </div>
+            )})}
+        </div>
+    );
+}
+
+export default Skills;
